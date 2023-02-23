0590cfa4611a7e3e5ee63d4ae066c904d95230ba
aandalkhanabro <aandalkhan16@gmail.com>
2023-02-22 20:00:48 -0800

    phase 1 - rough commit 3

--- src/main/ui/StroopGame.java
+++ src/main/ui/StroopGame.java
@@ -1,15 +1,5 @@
 package ui;
 
-import java.util.Scanner;
-
-import static ui.GamePanel.startGame;
-
 public class StroopGame {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        GamePanel game = new GamePanel();
-        GamePanel.makeFirstGame();
-
-    }
 }
